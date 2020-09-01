217269cc8b096dbc60edd8149d0b88ff70c11815
Ratha Prom <rathaprom@gmail.com>
2020-09-01 05:27:19 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,3 +6,7 @@ display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet
 puts "R: red: #{color[0]}"
 puts "O: orange: #{color[1]}"
 puts "Y: yellow: #{color[2]}"
+puts "Y: yellow: #{color[2]}"
+puts "Y: yellow: #{color[2]}"
+puts "Y: yellow: #{color[2]}"
+puts "Y: yellow: #{color[2]}"
