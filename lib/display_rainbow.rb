d47536e9b80662f4d07fa4f2c1e7d4cf808c6630
Ratha Prom <rathaprom@gmail.com>
2020-09-01 05:35:14 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,11 +4,3 @@ def greeting(name = "display rainbow")
   puts "display_rainbow, #{name}"
 end
 
-display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-puts "R: red: #{display[0]}"
-puts "O: orange: #{display[1]}"
-puts "Y: yellow: #{display[2]}"
-puts "G: green: #{display[3]}"
-puts "B: blue: #{display[4]}"
-puts "I: indigo: #{display[5]}"
-puts "V: violet: #{display[6]}"
